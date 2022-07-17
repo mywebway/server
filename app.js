aa866a626ca3e54e49355f0520498780af151b6e
Sergey <97333505+konoplitskii@users.noreply.github.com>
2022-07-17 11:47:06 +0300

    first add

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+
+    fs.readFile('./views/index.html', (err,data)=> {
+        res.write(data);
+        res.end();
+    })
+})
+
+
+server.listen('3211', ()=> {
+ console.log('Server start')
+})
+
+
+
